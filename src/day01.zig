d5b2f37f239f6c1121bc976e3e79b90ae593c747
sgarabaya <sgarabaya@sessionm.com>
2021-03-23 19:47:35 +0100

    Day 01

--- src/day01.zig
+++ src/day01.zig
@@ -3,6 +3,31 @@ const Allocator = std.mem.Allocator;
 const assert = std.debug.assert;
 const print = std.debug.print;
 
-pub fn main() !void {
+const numbers = @import("input01.zig").numbers;
 
+fn part1() void {
+    for (numbers) |number, i| {
+        for (numbers[i + 1 ..]) |next| {
+            if (number + next == 2020) {
+                print("{} * {} = {}\n", .{ number, next, number * next });
+            }
+        }
+    }
+}
+
+fn part2() void {
+    for (numbers) |first, i| {
+        for (numbers[i + 1 ..]) |second| {
+            for (numbers[i + 2 ..]) |third| {
+                if (first + second + third == 2020) {
+                    print("{} * {} * {} = {}\n", .{ first, second, third, first * second * third });
+                }
+            }
+        }
+    }
+}
+
+pub fn main() !void {
+    part1();
+    part2();
 }
